ae78cc8edc1bbfb48ade22bbe0e776fa6b10c1a9
Anna Suuronen <annasuuronen@icloud.com>
2024-12-07 19:41:34 +0200

    tehtävä 5.1 tehty

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import login from './services/login'
 
+const LoginForm = (props) => {
+  return (
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={props.handleLogin}>
+        <div>
+          username: <input type="text" value={props.username} name="username" onChange={({ target }) => props.setUsername(target.value)} />
+        </div>
+        <div>
+          password: <input type="password" value={props.password} name="password" onChange={({ target }) => props.setPassword(target.value)} />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
+
+const BlogsForm = (props) => {
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>{props.user.name} logged in</p>
+      {props.blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +44,31 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername("")
+      setPassword("")
+      console.log(user)
+    } catch (exception) {
+      window.alert("wrong password or username")
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <LoginForm username={username} password={password} setPassword={setPassword} setUsername={setUsername} handleLogin={handleLogin} />
+      </div>
+    )
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <BlogsForm blogs={blogs} user={user}/>
     </div>
   )
 }
