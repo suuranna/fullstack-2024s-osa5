490cfa32b6efc3471ba2e7b247eeccd82d83c56b
Anna Suuronen <annasuuronen@icloud.com>
2024-12-12 03:50:33 +0200

    tehtävä 5.10 tehty (myös 5.9 on tehty)

--- src/components/BlogsForm.jsx
+++ src/components/BlogsForm.jsx
@@ -1,10 +1,11 @@
 import Blog from './Blog'
 
 const BlogsForm = (props) => {
+  const sortedBlogs = props.blogs.sort((blog1, blog2) => blog2.likes - blog1.likes)
   return (
     <div>
       <h2>blogs</h2>
-      {props.blogs.map(blog =>
+      {sortedBlogs.map(blog =>
         <Blog key={blog.id} blog={blog} handleLiking={props.handleLiking} />
       )}
     </div>
