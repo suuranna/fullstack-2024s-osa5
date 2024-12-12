941cee2e55040acb4db55795eced1426dbcc3fa8
Anna Suuronen <annasuuronen@icloud.com>
2024-12-12 03:19:42 +0200

    tehtävä 5.7 tehty

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [viewMore, setViewMore] = useState(false)
+
+  const hideWhenVisible = { display: viewMore? 'none' : '' }
+  const showWhenVisible = { display: viewMore ? '' : 'none' }
+
+  const changeViewMore = () => {
+    setViewMore(!viewMore)
+  }
+
+  return (
+    <div className="blogStyle">
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author} <button onClick={changeViewMore}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} by {blog.author} <br/>
+        {blog.url} <br/>
+        likes: {blog.likes} <button>like</button> <br/>
+        {blog.user.name} <br/>
+        <button onClick={changeViewMore}>hide</button>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
