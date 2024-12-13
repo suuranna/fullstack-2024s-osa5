93006d78bb2935addb3de6ab4fc647d980c35757
Anna Suuronen <annasuuronen@icloud.com>
2024-12-13 16:24:06 +0200

    tehtävä 5.18 tehty

--- cypress/cypress/e2e/blog_app.cy.js
+++ cypress/cypress/e2e/blog_app.cy.js
@@ -6,12 +6,33 @@ describe('Blog app', function() {
       username: 'Maijalainen',
       password: 'salasana'
     }
-    cy.request('POST', 'http://localhost:3003/api/users/', user) 
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
     cy.visit('http://localhost:5173')
     cy.contains('log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('Maijalainen')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
+      cy.contains('Logged in succesfully! Welcome to blogapp, Maija Meikäläinen')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('Maijalainen')
+      cy.get('#password').type('12345')
+      cy.get('#login-button').click()
+      cy.contains('Wrong username or password')
+      cy.get('#password').clear()
+      cy.get('#username').type('2')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
+      cy.contains('Wrong username or password')
+    })
   })
 })
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,12 +6,23 @@ const LoginForm = (props) => {
       <h2>log in to application</h2>
       <form onSubmit={props.handleLogin}>
         <div>
-          username: <input type="text" value={props.username} name="username" onChange={({ target }) => props.setUsername(target.value)} />
+          username: <input 
+            type="text"
+            id='username'
+            value={props.username}
+            name="username"
+            onChange={({ target }) => props.setUsername(target.value)}
+          />
         </div>
         <div>
-          password: <input type="password" value={props.password} name="password" onChange={({ target }) => props.setPassword(target.value)} />
+          password: <input
+            type="password"
+            id='password'
+            value={props.password}
+            name="password" onChange={({ target }) => props.setPassword(target.value)}
+          />
         </div>
-        <button type="submit">login</button>
+        <button type="submit" id='login-button'>login</button>
       </form>
     </div>
   )
